8a9f6aaaac191cf6578105e2f0185b471cced401
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-11-01 23:20:31 +0530

    Create function overloading.cpp

---
+++ function overloading.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int volume(int s)//cube
+{
+	return s*s*s;
+}
+double volume(double r, int h)//cylinder
+{
+	return 3.4519*r*r*h;
+}
+long volume(long l,int b,int h)//rectangular box
+{
+	return l*b*h;
+	
+}
+
+int main()
+{
+	cout<<volume(10)<<"\n";
+	cout<<volume(2.5,8)<<"\n";
+	cout<<volume(100L,75,15)<<"\n";
+}
